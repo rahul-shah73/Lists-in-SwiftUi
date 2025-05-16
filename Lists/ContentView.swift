86001311aea8ee929625bd7da6348fa2b862223a
DakuGabbar73 <rahulshah887859@gmail.com>
2025-05-16 23:58:26 +0530

    Initial Commit

---
+++ Lists/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lists
+//
+//  Created by Rahul Shah on 16/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by Rahul Shah on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
